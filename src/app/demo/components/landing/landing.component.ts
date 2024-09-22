18bf698e2819fca26aad4eecc97cf33b15448656
calexgm.dev <calexgm.dev@gmail.com>
2024-09-22 14:23:58 -0500

    Diseño completo de la landing page

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -4,10 +4,20 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     selector: 'app-landing',
-    templateUrl: './landing.component.html'
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
 
     constructor(public layoutService: LayoutService, public router: Router) { }
+
+    teachers:any = [1,2,3,4,5,6];
+
+    scrollToElement(elementId: string): void {
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
     
 }
