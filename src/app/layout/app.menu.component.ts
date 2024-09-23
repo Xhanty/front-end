bfd4a382e84d0e1fc4cf44e6da6e29e8d9394bf5
calexgm.dev <calexgm.dev@gmail.com>
2024-09-22 21:48:15 -0500

    Diseño dashboard menu, topbar, mouse

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -21,12 +21,45 @@ export class AppMenuComponent implements OnInit {
                 ]
             },
             {
-                label: 'Plataforma',
+                label: 'Administrador',
                 items: [
-                    { label: 'Materias', icon: 'pi pi-fw pi-book'},
-                    { label: 'Tareas', icon: 'pi pi-fw pi-check-square'},
-                    { label: 'Perfil', icon: 'pi pi-fw pi-user'},
-                    { label: 'Cerrar Sesión', icon: 'pi pi-fw pi-sign-out'}
+                    { label: 'Materias', icon: 'pi pi-fw pi-book', routerLink: ['/']},
+                    { label: 'Roles', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Usuarios', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Estudiantes', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Asistencia', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Calificaciones', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                ]
+            },
+            {
+                label: 'Profesor',
+                items: [
+                    { label: 'Tareas (Proximamente)', icon: 'pi pi-fw pi-check-square', routerLink: ['/']},
+                    { label: 'Calificaciones', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Asistencia', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Reportes', icon: 'pi pi-fw pi-sign-out', routerLink: ['/']},
+                    { label: 'Estudiantes', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                ]
+            },
+            {
+                label: 'Estudiante',
+                items: [
+                    { label: 'Tareas (Proximamente)', icon: 'pi pi-fw pi-check-square', routerLink: ['/']},
+                    { label: 'Mis calificaciones y asistencia', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                ]
+            },
+            {
+                label: 'Acudientes',
+                items: [
+                    { label: 'Informe académico', icon: 'pi pi-fw pi-book', routerLink: ['/']},
+                ]
+            },
+            {
+                label: 'Otros',
+                items: [
+                    { label: 'Noticias', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Perfil', icon: 'pi pi-fw pi-user', routerLink: ['/']},
+                    { label: 'Cerrar Sesión', icon: 'pi pi-fw pi-sign-out', routerLink: ['/']}
                 ]
             },
         ];
